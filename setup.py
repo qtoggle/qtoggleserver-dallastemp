416d597dbef52f449846d991968a7c3ac3c29ded
Calin Crisan <ccrisan@gmail.com>
2019-12-15 22:43:04 +0200

    Add CI/Release workflow

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 
 from setuptools import setup, find_namespace_packages
 
+from qtoggleserver.dallastemp import VERSION
+
 
 setup(
     name='qtoggleserver-dallastemp',
-    version='1.0.0',
+    version=VERSION,
     description='Dallas DS18B20 temperature sensor support for qToggleServer',
     author='Calin Crisan',
     author_email='ccrisan@gmail.com',
